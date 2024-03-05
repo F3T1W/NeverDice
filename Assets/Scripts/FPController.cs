0ff0156342b2c6da1371ad2e248f9fdc7f5cc810
Vlad Matyushin <particular0010abyss@gmail.com>
2024-03-05 18:00:42 +0500

    Made test dice throw mech on LMB

--- Assets/Scripts/FPController.cs
+++ Assets/Scripts/FPController.cs
@@ -30,37 +30,37 @@ public class FPController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //#region Handles Movment
+        #region Handles Movment
 
-        //Vector3 forward = transform.TransformDirection(Vector3.forward);
-        //Vector3 right = transform.TransformDirection(Vector3.right);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
 
-        //// Press Left Shift to run
-        //bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        //float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        //float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
-        //float movementDirectionY = moveDirection.y;
-        //moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+        // Press Left Shift to run
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
-        //#endregion
+        #endregion
 
-        //#region Handles Jumping
+        #region Handles Jumping
 
-        //if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
-        //{
-        //    moveDirection.y = jumpPower;
-        //}
-        //else
-        //{
-        //    moveDirection.y = movementDirectionY;
-        //}
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        {
+            moveDirection.y = jumpPower;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
 
-        //if (!characterController.isGrounded)
-        //{
-        //    moveDirection.y -= gravity * Time.deltaTime;
-        //}
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
 
-        //#endregion
+        #endregion
 
         #region Handles Rotation
 
